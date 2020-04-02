d45e27ca4ad3875402d2a404b2515599733e5b7a
Dreycey <albindreycey@gmail.com>
2020-04-02 16:33:46 -0500

    now has plotting ability, to plot mutation counts per gene

--- mutation_finder.py
+++ mutation_finder.py
@@ -5,6 +5,7 @@ import json
 import os
 ## non-standard
 import matplotlib.pyplot as plt
+import numpy as np
 ## from this analysis
 from json_parser import nextstrain_analysis as ns_parser
 
@@ -28,20 +29,63 @@ def mutationfreq_to_plot(mut_dict):
              [GFP, M1A, 23]
             ]
     """
-    print("it is working hehe.")
     mut_count_array = []
     for protein in mut_dict:
-        print(protein)
+        #print(protein)
         for mutation, count in mut_dict[protein].items():
-            print('\t', mutation, count)
+            #print('\t', mutation, count)
             mut_count_array.append([protein, mutation, count])
     return mut_count_array
 
-
-
 # plotter (make as general as possible)
-def dynamic_bar_plot():
-    None
+def dynamic_bar_plot(input_array):
+    """
+    This function makes a plot for the frequency of mutations. 
+
+    INPUT:
+        [
+         [protein, mutation, count], 
+         [ATPsynthase, M1G, 15],
+         ...
+         [GFP, M1A, 23]
+        ]
+
+    OUTPUT: a PNG file with bar plots
+    """
+    # parameters
+    cutoff = 10
+    cols = 5
+    output_fig_name = "figout.png"
+    array = np.asarray(input_array)
+    sub_plot_names = set(array[:,0])
+    rows = int(round(len(sub_plot_names) / cols))
+    fig, axs = plt.subplots(rows,cols, figsize=(15,10))
+    print(round(len(sub_plot_names) / cols)*cols, len(sub_plot_names))
+    counter = 0
+    row_temp = -1
+    for sp_name in sub_plot_names:
+        col_temp = counter % cols
+        row_temp += 1 if col_temp == 0 else 0
+        #print(row_temp, col_temp)
+        sp_array = array[array[:,0] == sp_name][:cutoff]
+        mut_name = sp_array[:,1]
+        count_vec = list(map(int, sp_array[:,2]))
+        print(mut_name, count_vec)
+        print("before")
+        axs[row_temp, col_temp].bar(mut_name, count_vec, color = 'orange',
+                                    edgecolor='b')
+        axs[row_temp, col_temp].set_title(sp_name)
+        axs[row_temp, col_temp].tick_params(axis='x', labelrotation=90)
+        counter += 1
+    fig.suptitle('Mutational frequencies for each COVID-19 gene', size=20)
+    fig.subplots_adjust(hspace=0.5, wspace=0.5)
+    fig.savefig(output_fig_name, dpi=300, facecolor='w', edgecolor='b',
+        orientation='portrait', papertype=None, format=None,
+        transparent=True, pad_inches=1,
+        metadata=None)
+
+
+
 
 # main function that controls analysis.
 def main():
@@ -54,7 +98,7 @@ def main():
 
     # plotting
     NeededForPlotting = mutationfreq_to_plot(mut_freq_dict)
-    #dynamic_bar_plot(NeededForPlotting)
+    dynamic_bar_plot(NeededForPlotting)
 
     print("end main()")
 
